5e94fad9244b3780075253288686c603d20b7d97
tvz <tvz.041@gmail.com>
2021-03-01 15:54:58 +0700

    Добавлены функции сохранения/загрузки JSON на диск

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
+#include <QApplication>
+#include <QDebug>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+
 #include "AdressBookEntry.h"
 
-#include <QApplication>
+void saveJsonToFile(const QString &filename, const QJsonObject &json)
+{
+	QFile file(filename);
+
+	if (file.open(QFile::WriteOnly)) {
+		QJsonDocument document(json);
+		file.write(document.toJson());
+		file.close();
+	}
+}
+
+QJsonObject loadJsonFromFile(const QString &filename)
+{
+	QJsonObject json;
+	QFile file(filename);
+
+	if (file.open(QFile::ReadOnly)) {
+		QJsonDocument document = QJsonDocument::fromJson(file.readAll());
+		json = document.object();
+		file.close();
+	}
+
+	return json;
+}
+
 
 int main(int argc, char *argv[])
 {
